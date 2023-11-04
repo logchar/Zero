253cfd8841783f21372b01f7d4255db7d35fd043
logchar <1425926331@qq.com>
2023-11-04 11:23:38 +0800

    feat: 首页

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,11 @@
+
+
 window.addEventListener("click", () => {
     let scrolled_id = (document.documentElement.scrollTop) / (document.documentElement.clientHeight);
     scrolled_id = parseInt(scrolled_id) + 1;
     let screens = document.getElementsByClassName("screen");
-    document.documentElement.scrollTop = screens[scrolled_id].offsetTop;
+    $("html").animate({
+        scrollTop: screens[scrolled_id].offsetTop
+    }, 400)
     console.log(scrolled_id);
 })
